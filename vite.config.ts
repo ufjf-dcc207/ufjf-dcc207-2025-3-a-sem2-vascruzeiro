6cc33062d4e98571a829167630ff0faee9f827d2
RenatoNandes <renato.nandes@estudante.ufjf.br>
2025-12-09 20:02:44 -0300

    Ajeita gh pages pro seminario 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ufjf-dcc207-2025-3-a-sem01-vascode/",
+  base: "/ufjf-dcc207-2025-3-a-sem2-vascruzeiro/",
 })
